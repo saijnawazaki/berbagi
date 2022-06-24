6c5bb639f054232aad86429e0dc4cbfec0df972e
Julianto Chai <saijnawazaki@gmail.com>
2022-06-24 10:34:13 +0700

    Reconfig
    env for public is not safe?

--- config.example.php
+++ config.example.php
@@ -3,7 +3,7 @@
 define('APP_PATH', dirname(__FILE__));
 define('DB_PATH', APP_PATH.'/assets/berbagi.db');
 define('APP_URL', 'http://localhost/berbagi/');
-define('MAFURA_URL', 'https://manastudio.id/repo/public/manastudio/mafura/');  
+define('MAFURA_URL', 'https://manastudio.id/repo/public/manastudio/mafura/');
 
 date_default_timezone_set('Asia/Jakarta');
 
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require 'config.php';
+require 'config_engine.php';
 
 session_start();
 
